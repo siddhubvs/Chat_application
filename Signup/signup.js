4276f465704637313e7d56b14f4461adf8d87e25
Siddhartha Bolla <siddhu.bvs@gmail.com>
2023-04-11 18:31:00 +0530

    added signup form backend

--- Signup/signup.js
+++ Signup/signup.js
@@ -16,10 +16,14 @@ async function signup(e){
     if(response.status===201){
        document.body.innerHTML+=`<br><div style="color:red;text-align:center;">${response.data.message}</div`
     }
+    else if(response.status===404){
+        document.body.innerHTML+=`<br><div style="color:red;text-align:center;">${response.data.err}</div`
+    }
     
     }
     catch(err){
         document.body.innerHTML+=`<br><div style="color:red;text-align:center;">'${err}</div`;
+        console.log(err.data.err);
     }
 
 }
--- controllers/user.js
+++ controllers/user.js
@@ -14,6 +14,9 @@ function isStringInvalid(str){
     if(isStringInvalid(name) || isStringInvalid(email) || isStringInvalid(phone) || isStringInvalid(password)){
         return res.status(400).json({err:"Bad paramters : Something is missing"})
     }
+    await User.findOne({where:{email:email}}).then(()=>{
+        return res.status(404).json({err:"User already exits"})
+    })
     try{
     bcrypt.hash(password,10,async(err,hash)=>{
     const data=await User.create({name,email,phone,password:hash})
